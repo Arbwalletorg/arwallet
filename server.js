6ab95aed1a13a56dd3f9e55b796b385e8a6057a9
Arbwalletorg <verifiedyour8@gmail.com>
2026-05-19 02:36:57 +0530

    Added login functionality

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post("/login", async (req, res) => {
 
         await newUser.save();
 
-        console.log("User Saved Successfully ✅");
+        console.log("congrats your reward is now active ✅ ");
 
         res.json({
             success: true,
